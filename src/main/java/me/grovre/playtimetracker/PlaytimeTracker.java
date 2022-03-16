e1bb22d38448742efb482ea4cea9fc7da57b823e
Grovre <50428844+Grovre@users.noreply.github.com>
2022-03-15 22:31:16 -0500

    Initial commit

---
+++ src/main/java/me/grovre/playtimetracker/PlaytimeTracker.java
@@ -0,0 +1,17 @@
+package me.grovre.playtimetracker;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class PlaytimeTracker extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
